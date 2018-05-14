776a54b0d7bc143e02f31f8a37a5b9ce73162bf9
Aaron Chang <aaron.chang@quadas.com>
2018-05-14 11:21:27 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+This repository is for web prototyping. It auto compiles scss, triggers reload when html/css changes.
+
+### Commands
+1. Install all pacakges
+
+    `npm install`
+
+1. Start server and watch files
+
+    `npm run watch`
---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const changed = require("changed");
+const grep = require("gulp-grep");
+const browserSync = require("browser-sync");
+const concat = require("gulp-concat");
+const newer = require("gulp-newer");
+
+const paths = {
+  dest: {
+    default: "./build",
+    images: "./build/images"
+  },
+  src: {
+    sass: ["./assets/**/*.scss"],
+    images: "./assets/img/**/*",
+    html: "./"
+  }
+};
+
+gulp.task("sass", () => {
+  return gulp
+    .src(paths.src.sass)
+    .pipe(sass())
+    .pipe(gulp.dest(paths.dest.default))
+    .pipe(browserSync.reload({ stream: true }));
+});
+
+gulp.task("img:copy", () => {
+  return gulp
+    .src(paths.src.images)
+    .pipe(newer(paths.dest.images))
+    .pipe(gulp.dest(paths.dest.images));
+});
+
+gulp.task("server", () => {
+  browserSync.init({
+    server: { baseDir: [paths.src.html, paths.dest.default] },
+    browser: "google chrome"
+  });
+});
+
+gulp.task("watch", () => {
+  gulp.watch(paths.src.sass, gulp.series("sass"));
+  gulp.watch(paths.images, gulp.series("img:copy"));
+});
+
+gulp.task("default", gulp.parallel("sass", "img:copy", "watch", "server"));
